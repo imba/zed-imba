139250d2dbfee54068c782669a66ae33f5e96781
Sindre Aarsaether <sindre.aarsaether@gmail.com>
2026-05-10 10:49:19 +0200

    Add Imba tag symbols to outline

--- languages/imba/outline.scm
+++ languages/imba/outline.scm
@@ -1,14 +1,41 @@
 (class_declaration
+  [
+    "class"
+    "interface"
+    "mixin"
+  ] @context
   name: (class_name) @name) @item
 
 (class_expression
+  "class" @context
   name: (class_name) @name) @item
 
 (tag_declaration
+  [
+    "extend"
+    "global"
+    "local"
+    "declare"
+    "abstract"
+  ]? @context
+  "tag" @context
   name: (tag_type) @name) @item
 
 (method_declaration
+  [
+    "def"
+    "get"
+    "set"
+    "constructor"
+  ] @context
   name: (_) @name) @item
 
 (field_declaration
+  [
+    "prop"
+    "attr"
+    "let"
+    "const"
+    "isa"
+  ] @context
   name: (_) @name) @item
